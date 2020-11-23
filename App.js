0ea24e7c86452b5c504ae52860198b08dbf1b325
Zubair Rauf <zubair@zubairrauf.com>
2020-11-23 13:33:25 +0100

    basic setup and todo list

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
+import {Button} from 'native-base'
+import {FontAwesome} from '@expo/vector-icons'
+
+const itemArray = new Array(25).fill('empty')
 
 export default function App() {
+  const [randomNumber, setRandomNumber] = useState('')
+  useEffect(() => {
+   generateRandomNumber()
+  }, [])
+
+  const generateRandomNumber = () => {
+    setRandomNumber(Math.floor(Math.random() * 25 ))
+  }
+
+  const scratchItem = () => {
+    //TODO: decide lucky or not
+  }
+
+  const scratchItemIcon = () => {
+    //TODO: Find right icon
+  }
+
+  const scratchItemColor = () => {
+    //TODO: Find right color
+  }
+
+  const showAllItems = () => {
+    //TODO: reveal all icons
+  }
+
+  const resetGame = () => {
+    //TODO: reset the game
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Scratch and win</Text>
+      <Text>{randomNumber}</Text>
       <StatusBar style="auto" />
     </View>
   );
